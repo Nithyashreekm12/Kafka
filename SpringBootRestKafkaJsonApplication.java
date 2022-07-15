f4a61ad9a72da1a6663132bb5825500485dfb0ea
Nithyashreekm12 <95600494+Nithyashreekm12@users.noreply.github.com>
2022-07-15 18:20:55 +0530

    Add files via upload

---
+++ SpringBootRestKafkaJsonApplication.java
@@ -0,0 +1,13 @@
+package dk.kmd.cnap.examples.spring.boot.rest.kafka.json.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** The Spring Boot Application class */
+
+@SpringBootApplication
+public class SpringBootRestKafkaJsonApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRestKafkaJsonApplication.class, args);
+  }
+}
